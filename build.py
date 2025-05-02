174a8a0d9b74f39dbea35882c97485af005c4a45
julio <julio@simbiotica.coop>
2025-05-02 18:39:43 +0200

    Añadido el Bombartaker Mejorado el sistema para incluir bolos realizados

--- build.py
+++ build.py
@@ -9,10 +9,24 @@ with open('content.json', 'r') as file_content:
     content = file_content.read()
     data = json.loads(content)
 
-with open(f'index.mustache.html', 'r') as f:
-    web = chevron.render(f, data)
+    # Preprocessing data
+    for index, item in enumerate(data['performances']):
+        item['title_cover'] = item['title'].replace("-", "<p>")
+        if item['url']:
+            item['title_link'] = f"<b><a href=\'{item['url']}\' target=\'_blank\' style=\'color:#ad4582\'>{item['title']}</a></b>"
+        else:
+            item['title_link'] = f"<b>{item['title']}</b>"
 
-public_dir = 'public'
+        left = index % 2 == 0
+        item['color'] = 'orange' if left else 'purple'
+        item['position'] = 'left' if left else 'right'
 
-with open(f'{public_dir}/index.html', 'w') as out:
-    out.write(web)
+    with open(f'index.mustache.html', 'r') as f:
+        web = chevron.render(f, data)
+
+        public_dir = 'public'
+
+        with open(f'{public_dir}/index.html', 'w') as out:
+            out.write(web)
+
+            print('Finished!')
